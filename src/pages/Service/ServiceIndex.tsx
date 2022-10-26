3b91dabba3718fcbaf4ecf33c8668f52e454c4a4
link2fun <ethuner92@gmail.com>
2022-10-26 21:52:45 +0800

    feat: 初步完成 服务->网络连通性测试

--- src/pages/Service/ServiceIndex.tsx
+++ src/pages/Service/ServiceIndex.tsx
@@ -1,13 +1,11 @@
 import { Col, Row } from 'antd';
 import ServiceNameTest from '@/pages/Service/components/ServiceNameTest';
 import NetworkTest from '@/pages/Service/components/NetworkTest';
+import { PageContainer } from '@ant-design/pro-components';
+
 const ServiceIndex = () => {
   return (
-    <div className="p-4">
-      <div className={'flex justify-between items-center my-3 '}>
-        <div className={'font-bold text-xl'}>站点维护</div>
-        <div></div>
-      </div>
+    <PageContainer breadcrumbRender={false} waterMarkProps={{ content: '' }}>
       <Row gutter={[16, 32]}>
         <Col span={24} sm={24} md={12} lg={12} xl={8} xxl={8}>
           <ServiceNameTest />
@@ -16,7 +14,7 @@ const ServiceIndex = () => {
           <NetworkTest />
         </Col>
       </Row>
-    </div>
+    </PageContainer>
   );
 };
 
