28e743f8d501c7c90fd0452110055dfe8f903b6c
link2fun <ethuner92@gmail.com>
2022-10-31 21:17:07 +0800

    feat: 增加 媒体库配置

--- src/pages/Setting/Library/SettingLibraryIndex.tsx
+++ src/pages/Setting/Library/SettingLibraryIndex.tsx
@@ -1,15 +1,93 @@
-import { PageContainer } from '@ant-design/pro-components';
+import { ModalForm, PageContainer, ProFormItem } from '@ant-design/pro-components';
 import SettingLibraryItem from '@/pages/Setting/Library/components/SettingLibraryItem';
+import { useEffect, useState } from 'react';
+import { postForm } from '@/utils/request';
+import PathSelector from '@/components/PathSelector';
+import { message } from 'antd';
+
+export type LibraryItemConfig = {
+  name: string;
+  key: string;
+  paths: string[];
+};
 
 const SettingLibraryIndex = () => {
+  const [libraryItems, setLibraryItems] = useState<LibraryItemConfig[]>([]);
+  const [open, setOpen] = useState(false);
+  const [activeKey, setActiveKey] = useState<string>('');
+
+  const loadConfigInfo = () => {
+    postForm('/api/v1/config/info', {}).then((config) => {
+      const { media } = config;
+      const { movie_path, tv_path, anime_path, unknown_path } = media;
+      setLibraryItems([
+        { name: '电影', key: 'movie_path', paths: movie_path || [] },
+        { name: '电视剧', key: 'tv_path', paths: tv_path || [] },
+        { name: '动漫', key: 'anime_path', paths: anime_path || [] },
+        { name: '未识别', key: 'unknown_path', paths: unknown_path || [] },
+      ]);
+    });
+  };
+
+  useEffect(() => {
+    loadConfigInfo();
+  }, []);
+
   return (
     <PageContainer breadcrumbRender={false} waterMarkProps={{ content: '' }}>
       <div>
-        <SettingLibraryItem />
-        <SettingLibraryItem />
-        <SettingLibraryItem />
-        <SettingLibraryItem />
+        {libraryItems.map((item) => {
+          return (
+            <SettingLibraryItem
+              key={item.key}
+              item={item}
+              onAdd={(key: string) => {
+                console.log('add', key);
+                setActiveKey(item.key);
+                setOpen(true);
+              }}
+              onRemove={async (path) => {
+                console.log('remove', item.key, path);
+                await postForm('/api/v1/config/directory', {
+                  oper: 'sub',
+                  key: 'media.' + item.key,
+                  value: path,
+                });
+                message.success('移除成功');
+                await loadConfigInfo();
+              }}
+            />
+          );
+        })}
       </div>
+
+      <ModalForm
+        title="目录设置"
+        open={open}
+        submitter={{
+          searchConfig: {
+            submitText: '确认',
+            resetText: '取消',
+          },
+        }}
+        onFinish={async (values) => {
+          console.log({ ...values, key: activeKey });
+          await postForm('/api/v1/config/directory', {
+            oper: 'add',
+            key: 'media.' + activeKey,
+            value: values.path,
+          });
+          message.success('添加成功');
+          await loadConfigInfo();
+          await setOpen(false);
+          await setActiveKey('');
+          return true;
+        }}
+      >
+        <ProFormItem name={'path'} label={'目录'} rules={[{ required: true }]}>
+          <PathSelector />
+        </ProFormItem>
+      </ModalForm>
     </PageContainer>
   );
 };
--- src/pages/Setting/Library/components/SettingLibraryItem.tsx
+++ src/pages/Setting/Library/components/SettingLibraryItem.tsx
@@ -1,28 +1,44 @@
 import { IconPlus, IconX } from '@tabler/icons';
+import React from 'react';
+import { LibraryItemConfig } from '@/pages/Setting/Library/SettingLibraryIndex';
 
-const SettingLibraryItem = () => {
+type SettingLibraryItemProps = {
+  /** 配置项 */
+  item: LibraryItemConfig;
+  /** 监听新增按钮点击 */
+  onAdd?: (key: string) => void;
+  /** 监听删除按钮点击 */
+  onRemove?: (path: string) => void;
+};
+
+const SettingLibraryItem: React.FC<SettingLibraryItemProps> = ({
+  item,
+  onAdd = () => {},
+  onRemove = () => {},
+}) => {
   return (
     <div className={' rounded-xl shadow-md mb-4'}>
       <div className={'flex justify-between items-center bg-white px-3 py-4 rounded-t-xl'}>
-        <div className={'font-bold text-2xl'}>电影</div>
+        <div className={'font-bold text-2xl'}>{item.name}</div>
         <div className={'mr-2'}>
-          <IconPlus className={'bg-[#1b5ba7] rounded'} color={'white'} size={32} />
+          <IconPlus
+            className={'bg-[#1b5ba7] rounded cursor-pointer'}
+            color={'white'}
+            size={32}
+            onClick={() => onAdd(item.key)}
+          />
         </div>
       </div>
       <div className={'text-gray-600 font-bold bg-[#f2f3f4]  p-3 '}>目录</div>
       <div className={'bg-white  divide-gray-200 divide-y rounded-b-xl'}>
-        <div className={'row flex  justify-between items-center p-3 '}>
-          <div>/nastools/电影</div>
-          <div>
-            <IconX color={'#206bc4'} />
+        {item.paths.map((dir) => (
+          <div className={'row flex  justify-between items-center p-3 '} key={dir}>
+            <div>{dir}</div>
+            <div>
+              <IconX color={'#206bc4'} onClick={() => onRemove(dir)} className={'cursor-pointer'} />
+            </div>
           </div>
-        </div>
-        <div className={'row flex  justify-between items-center p-3 '}>
-          <div>/nastools/电影</div>
-          <div>
-            <IconX color={'#206bc4'} />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
